0c14ce6024fb30d230c3da3410a33f60062b75d4
Samuel Tjhia <110407645+samtjhia@users.noreply.github.com>
2025-11-25 04:14:30 -0500

    PLEASE WORK

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -3,11 +3,13 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
     port: 587,
-    secure: false, // true for 465, false for other ports
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
-    }
+    },
+    // Force IPv4 to avoid IPv6 timeouts in cloud environments
+    family: 4
 });
 
 const sendEmail = async (to, subject, html) => {
